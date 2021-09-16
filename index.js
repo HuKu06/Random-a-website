e67b295aeda006fdc6504f01cda2e24d55eadbf0
HuKu06 <88894443+HuKu06@users.noreply.github.com>
2021-09-16 15:48:04 +0545

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let exnum = prompt('Single - (yes/no?)')
-
-
-if (exnum == 'yes'){
-        window.location.href = 'https://tinder.com/';
-}else{
-        window.location.href = 'https://hackspirit.com/signs-of-cheating/';
-}
