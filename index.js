e8bf5cfa757dc46bcf67f1e10b35a1e92105031f
HuKu06 <88894443+HuKu06@users.noreply.github.com>
2021-09-16 15:48:44 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let exnum = prompt('Single - (yes/no?)')
+
+
+if (exnum == 'no'){
+        window.location.href = 'https://hackspirit.com/signs-of-cheating/';
+}else{
+        window.location.href = 'https://tinder.com/';
+}
