8106e6d35518180cd600b3e5019f8d9901c8ac16
HuKu06 <88894443+HuKu06@users.noreply.github.com>
2021-09-17 12:16:45 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let num = prompt('Enter Your Number')
+
+if (num % 2 == 0)
+{
+        alert('Your Number is Even')
+}else{
+        alert('Your Number is Odd')
+}
+/*let exnum = prompt('Single - (yes/no?)')
+
+
+if (exnum == 'no'){
+        window.location.href = 'https://hackspirit.com/signs-of-cheating/';
+}else{
+        window.location.href = 'https://tinder.com/';
+}*/
